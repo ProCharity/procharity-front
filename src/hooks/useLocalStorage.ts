901c6fb6cc1a3079af831d5301394fe804701bc8
Miran Kazaryan <mir12344@mail.ru>
2022-12-26 23:43:08 +0300

    useLocalStorage import

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 function useLocalStorage<T>(key: string, initialValue: T) {
   // State to store our value
